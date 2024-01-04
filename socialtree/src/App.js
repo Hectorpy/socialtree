e07dbf7cd0961d1e921e9ef4abc9c9ee5b066f74
Heitor Mota <128197648+Hectorpy@users.noreply.github.com>
2024-01-04 17:40:23 -0300

    Enviando projeto

--- socialtree/src/App.js
+++ socialtree/src/App.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import logo from './image0.jpeg';
-import otherImage from './inter1.png'; // Adicione o caminho correto da outra imagem
+import newImage from './charadaprin.png'; // Adicione o caminho correto da nova imagem
+import newImage2 from './charadaprin.png';
 import './styles.scss';
 
 function App() {
-  const numberOfImages = 20; // Aumentando o número de imagens
-  const maxImageSize = 100; // Aumentando o tamanho máximo das imagens em pixels
-  const minDistance = 50; // Definindo a distância mínima entre as imagens
-
   const [windowSize, setWindowSize] = useState({
     width: window.innerWidth,
     height: window.innerHeight,
@@ -28,74 +25,6 @@ function App() {
     };
   }, []);
 
-  const generateRandomPosition = (existingImages) => {
-    let position;
-    let isOverlap;
-
-    do {
-      isOverlap = false;
-
-      position = {
-        left: Math.random() * (windowSize.width - maxImageSize),
-        top: Math.random() * (windowSize.height - maxImageSize),
-      };
-
-      for (const img of existingImages) {
-        const xDiff = Math.abs(position.left - img.left);
-        const yDiff = Math.abs(position.top - img.top);
-
-        const distance = Math.sqrt(xDiff * xDiff + yDiff * yDiff);
-
-        if (distance < minDistance) {
-          isOverlap = true;
-          break;
-        }
-      }
-    } while (isOverlap);
-
-    return position;
-  };
-
-  const renderOtherImages = () => {
-    const images = [];
-    const imageElements = []; // Adicionado para armazenar os elementos da imagem
-
-    for (let i = 0; i < numberOfImages; i++) {
-      const position = generateRandomPosition(images);
-      const size = generateRandomSize();
-
-      images.push({
-        left: position.left,
-        top: position.top,
-      });
-
-      imageElements.push( // Empurrando o elemento da imagem para imageElements em vez de retornar
-        <div
-          key={i}
-          className="Other-image-container"
-          style={{ position: 'absolute', left: position.left, top: position.top }}
-        >
-          <img
-            src={otherImage}
-            className="Other-image"
-            alt={`outra imagem ${i}`}
-            style={{ width: size.width, height: size.height }}
-          />
-        </div>
-      );
-    }
-
-    return imageElements; // Retornando todos os elementos da imagem após o loop
-  };
-
-  const generateRandomSize = () => {
-    const size = Math.random() * maxImageSize + 50; // Ajustando o tamanho mínimo
-    return {
-      width: size,
-      height: size,
-    };
-  };
-
   return (
     <div className="App">
       <header className="App-header">
@@ -103,8 +32,13 @@ function App() {
           <img src={logo} className="App-logo" alt="logo" />
         </div>
 
-        {/* Renderizando várias imagens semelhantes à Other-image */}
-        {renderOtherImages()}
+        <div className="New-image-container" style={{ position: 'absolute', left: 0 }}>
+          <img src={newImage} className="New-image" alt="new image" />
+        </div>
+
+        <div className="New-image-container" style={{ position: 'absolute', right: -35}}>
+          <img src={newImage2} className="New-image" alt="new image" />
+        </div>
 
         <h1>
           Entre e concorra com mais de 800 pessoas !!
